4aae45576cac67583dd8cc15b0b5f36461fd62c0
Brunin12 <brunoparreira77@gmail.com>
2023-12-03 18:54:33 -0300

    sweet alert in default/components/layout/_js implementation

--- app/Views/default/components/layout/_js.php
+++ app/Views/default/components/layout/_js.php
@@ -4,45 +4,35 @@
 <script src="<?= base_url('assets/plugins/jquery-ui/jquery-ui.min.js') ?>"></script>
 <!-- Bootstrap 4 -->
 <script src="<?= base_url('assets/plugins/bootstrap/js/bootstrap.bundle.min.js') ?>"></script>
-<!-- ChartJS -->
-<script src="<?= base_url('assets/plugins/chart.js/Chart.min.js') ?>"></script>
-<!-- Sparkline -->
-<script src="<?= base_url('assets/plugins/sparklines/sparkline.js') ?>"></script>
-<!-- JQVMap -->
-<script src="<?= base_url('assets/plugins/jqvmap/jquery.vmap.min.js') ?>"></script>
-<script src="<?= base_url('assets/plugins/jqvmap/maps/jquery.vmap.usa.js') ?>"></script>
-<!-- jQuery Knob Chart -->
-<script src="<?= base_url('assets/plugins/jquery-knob/jquery.knob.min.js') ?>"></script>
-<!-- daterangepicker -->
-<script src="<?= base_url('assets/plugins/moment/moment.min.js') ?>"></script>
-<script src="<?= base_url('assets/plugins/daterangepicker/daterangepicker.js') ?>"></script>
-<!-- Tempusdominus Bootstrap 4 -->
-<script src="<?= base_url('assets/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js') ?>"></script>
-<!-- Summernote -->
-<script src="<?= base_url('assets/plugins/summernote/summernote-bs4.min.js') ?>"></script>
+
 <!-- overlayScrollbars -->
 <script src="<?= base_url('assets/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js') ?>"></script>
 <!-- AdminLTE App -->
 <script src="<?= base_url('assets/dist/js/adminlte.js') ?>"></script>
 
+
 <script>
     $(() => {
         $('.navbar').hide().show(750)
-    
+
         if (localStorage.getItem('dark-mode') === 'enabled') {
             $('body').addClass('dark-mode');
+            $('#toogleMode i').removeClass('fa-moon').addClass('fa-sun');
         }
 
-    
+
         $('#toogleMode').on('click', function() {
             if ($('body').hasClass('dark-mode')) {
                 $('body').removeClass('dark-mode');
+                $('#toogleMode i').removeClass('fa-sun').addClass('fa-moon');
                 localStorage.setItem('dark-mode', 'disabled');
             } else {
                 $('body').addClass('dark-mode');
+                $('#toogleMode i').removeClass('fa-moon').addClass('fa-sun');
                 localStorage.setItem('dark-mode', 'enabled');
             }
         });
+
         if ('serviceWorker' in navigator) {
             window.addEventListener('load', () => {
                 navigator.serviceWorker.register('/sw.js')
@@ -56,3 +46,33 @@
         }
     })
 </script>
+
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+<!-- Link para o CSS do SweetAlert2 via CDN -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">
+
+
+<?php if (session()->has('success')) : ?>
+    <script>
+        $(() => {
+            Swal.fire({
+                title: 'Sucesso!',
+                text: '<?= session('success') ?>',
+                icon: 'success',
+                showConfirmButton: false,
+                timer: 1500
+            });
+        });
+    </script>
+<?php elseif (session()->has('error')) : ?>
+    <script>
+        $(() => {
+            Swal.fire({
+                title: 'Erro!',
+                text: '<?= session('error') ?>',
+                icon: 'error',
+                showConfirmButton: true
+            });
+        });
+    </script>
+<?php endif; ?>
